950907519679094cc39fb1c77348688e6deb10c2
Maximilian Mörth <maxi.moerth@outlook.com>
2023-10-03 16:11:42 +0200

    removed regex implementation in IPersmissions interface

--- PaperlessRestService.BusinessLogic.Interfaces/IPermissions.cs
+++ PaperlessRestService.BusinessLogic.Interfaces/IPermissions.cs
@@ -1,6 +1,4 @@
-﻿using System.Text.RegularExpressions;
-
-namespace PaperlessRestService.BusinessLogic.Interfaces
+﻿namespace PaperlessRestService.BusinessLogic.Interfaces
 {
     public interface IPermissions
     {
